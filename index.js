225968afdbd56cb74f43a875a087e005a95490b8
Ashish <1ashishthakur2002@gmail.com>
2025-06-30 00:41:10 +0530

    toda sa modified

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 import express from 'express';
+import dotenv from 'dotenv';
 
 
+
+dotenv.config();
 const app = express();
+app.use(express.json());
 
 
-const PORT = 4003;
+app.get("/", (req, res) => {
+    res.send("Sample API is working");
+})
+
+const PORT = process.env.PORT;
 
 
 app.listen(PORT, () => {
